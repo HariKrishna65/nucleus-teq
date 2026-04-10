dd74ba38f00196274a4e65c83e5267fc1e8dd0b8
HariKrishna65 <thondapu.harikrishna999@gmail.com>
2026-04-10 17:33:28 +0530

    Tested all APIs and verified functionality

--- HariKrishna_java_training/session2/user-notification-system/src/main/java/com/example/usernotificationsystem/UserNotificationSystemApplication.java
+++ HariKrishna_java_training/session2/user-notification-system/src/main/java/com/example/usernotificationsystem/UserNotificationSystemApplication.java
@@ -1,4 +1,4 @@
-package com.example.user_notification_system;
+package com.example.usernotificationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
