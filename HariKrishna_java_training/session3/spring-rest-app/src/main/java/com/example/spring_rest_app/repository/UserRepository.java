e784593620317609dd3d3784cf4a7d3f42bed233
Tondapu Harikrishna <155811883+HariKrishna65@users.noreply.github.com>
2026-04-16 23:56:45 +0530

    added userrepository

--- HariKrishna_java_training/session3/spring-rest-app/src/main/java/com/example/spring_rest_app/repository/UserRepository.java
+++ HariKrishna_java_training/session3/spring-rest-app/src/main/java/com/example/spring_rest_app/repository/UserRepository.java
@@ -1,28 +1,28 @@
-// package com.example.spring_rest_app.repository;
+package com.example.spring_rest_app.repository;
 
-// import com.example.spring_rest_app.model.User;
-// import org.springframework.stereotype.Repository;
-// import java.util.ArrayList;
-// import java.util.List;
+import com.example.spring_rest_app.model.User;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
 
-// @Repository
-// public class UserRepository {
+@Repository
+public class UserRepository {
 
-//     private final List<User> users = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
 
-//     public UserRepository() {
-//         users.add(new User(1L, "Priya", 25, "USER"));
-//         users.add(new User(2L, "Rahul", 30, "ADMIN"));
-//         users.add(new User(3L, "Anjali", 28, "USER"));
-//         users.add(new User(4L, "Kiran", 30, "USER"));
-//         users.add(new User(5L, "Arjun", 35, "ADMIN"));
-//     }
+    public UserRepository() {
+        users.add(new User(1L, "Priya", 25, "USER"));
+        users.add(new User(2L, "Rahul", 30, "ADMIN"));
+        users.add(new User(3L, "Anjali", 28, "USER"));
+        users.add(new User(4L, "Kiran", 30, "USER"));
+        users.add(new User(5L, "Arjun", 35, "ADMIN"));
+    }
 
-//     public List<User> getAllUsers() {
-//         return users;
-//     }
+    public List<User> getAllUsers() {
+        return users;
+    }
 
-//     public void deleteById(Long id) {
-//         users.removeIf(user -> user.getId().equals(id));
-//     }
-// }
+    public void deleteById(Long id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
+}
