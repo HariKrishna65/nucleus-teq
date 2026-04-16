131b844529cca584dead7aa38374a59616ce6253
Tondapu Harikrishna <155811883+HariKrishna65@users.noreply.github.com>
2026-04-16 23:58:06 +0530

    Implement GlobalExceptionHandler for centralized error handling
    Added a global exception handler to manage exceptions across the application.

--- HariKrishna_java_training/session3/spring-rest-app/src/main/java/com/example/spring_rest_app/exception/GlobalExceptionHandler.java
+++ HariKrishna_java_training/session3/spring-rest-app/src/main/java/com/example/spring_rest_app/exception/GlobalExceptionHandler.java
@@ -1,1 +1,14 @@
+package com.example.spring_rest_app.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handle(Exception ex) {
+        return ResponseEntity.badRequest()
+                .body("Error: " + ex.getMessage());
+    }
+}
