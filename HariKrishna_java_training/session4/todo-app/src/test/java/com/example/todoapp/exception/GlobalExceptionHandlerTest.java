c0b2a37a9cfb7362afd4bd1ec89a21370d6eb96e
HariKrishna65 <thondapu.harikrishna999@gmail.com>
2026-04-20 14:08:45 +0530

    Fix GlobalExceptionHandlerTest void method stub syntax

--- HariKrishna_java_training/session4/todo-app/src/test/java/com/example/todoapp/exception/GlobalExceptionHandlerTest.java
+++ HariKrishna_java_training/session4/todo-app/src/test/java/com/example/todoapp/exception/GlobalExceptionHandlerTest.java
@@ -11,6 +11,7 @@ import com.example.todoapp.service.TodoService;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,8 +36,9 @@ class GlobalExceptionHandlerTest {
 
     @Test
     void delete_whenNotFound_returns404() throws Exception {
-        when(todoService.deleteTodo(anyLong()))
-                .thenThrow(new ResourceNotFoundException("Todo not found"));
+        doThrow(new ResourceNotFoundException("Todo not found"))
+                .when(todoService)
+                .deleteTodo(anyLong());
 
         mockMvc.perform(delete("/todos/999"))
                 .andExpect(status().isNotFound());
